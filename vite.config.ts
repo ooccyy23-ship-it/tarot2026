947a405456d36a53b055849af1e13cc602c7b3cc
ooccyy23-ship-it <ooccyy.23@gmail.com>
2026-07-21 21:03:52 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+
+  // GitHub Pages Repository 名稱
+  base: "/tarot2026/",
+
   test: {
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
